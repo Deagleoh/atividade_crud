89e8d0d4edbb9fbaad0e6f5e13bb13d831a0ea97
LorranDC <you@example.com>
2024-06-10 20:24:32 -0300

    in

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Começar Cadastro</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        a {
+            text-decoration: none;
+            color: #1a73e8;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+        .directory, .file {
+            margin: 5px 0;
+        }
+        .back-link {
+            margin-bottom: 20px;
+        }
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-top: 20px;
+            font-size: 16px;
+            color: #fff;
+            background-color: #1a73e8;
+            text-align: center;
+            border-radius: 5px;
+            text-decoration: none;
+            cursor: pointer;
+        }
+        .button:hover {
+            background-color: #0059c1;
+        }
+    </style>
+</head>
+<body>
+<h1>Começar Cadastro</h1>
+<div>
+    <a href="/atividade_crud/publico/user_create.php" class="button">Criar Usuário</a>
+</div>
+</body>
+</html>
